807357715a732bbfbcde9d20cd99d974c6023e37
Lydianne <nlyly2k4@gmail.com>
2026-03-23 13:40:49 -0300

    il y avait des erreus au niveau de la definition du model donc j'ai fait des changement encore

--- Models/WeatherResponse.cs
+++ Models/WeatherResponse.cs
@@ -1,32 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Projet2.Models
 {
     public class WeatherResponse
     {
+        [JsonPropertyName("name")]
         public string? Name { get; set; }
+
+        [JsonPropertyName("main")]
         public MainInfo? Main { get; set; }
+
+        [JsonPropertyName("weather")]
         public List<WeatherInfo>? Weather { get; set; }
+
+        [JsonPropertyName("wind")]
         public WindInfo? Wind { get; set; }
     }
 
     public class MainInfo
     {
+        [JsonPropertyName("temp")]
         public double Temp { get; set; }
+
+        // On conserve le nom utilisé dans la vue (Feels_Like) et on mappe le JSON "feels_like"
+        [JsonPropertyName("feels_like")]
         public double Feels_Like { get; set; }
+
+        [JsonPropertyName("humidity")]
         public int Humidity { get; set; }
     }
 
     public class WeatherInfo
     {
+        [JsonPropertyName("description")]
         public string? Description { get; set; }
+
+        [JsonPropertyName("icon")]
+        public string? Icon { get; set; }
     }
 
     public class WindInfo
     {
+        [JsonPropertyName("speed")]
         public double Speed { get; set; }
     }
 }
